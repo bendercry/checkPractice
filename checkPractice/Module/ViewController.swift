74a566f20caad9c44251e8110683fd2e57082e16
Bendersky Kirill <bendersky.kirill@gmail.com>
2021-06-28 12:23:12 +0400

    Replace example text to data from weather api

--- checkPractice/Module/ViewController.swift
+++ checkPractice/Module/ViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
+struct Headline {
+    var id : Int
+    var date : Date
+    var title : String
+    var text : String
+    var image : String
+}
+
 class ViewController: UIViewController, UIGestureRecognizerDelegate,UITableViewDelegate,UITableViewDataSource, NavigationBarDelegate{
     
     
@@ -16,6 +24,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate,UITableViewD
     @IBOutlet weak var customNavigationBar: NavigationBar!
     
     var forecastArray:[Weather] = [Weather]()
+    var copyForecast:[Weather] = [Weather]()
     var flag = false
     let images: [UIImage] = [ #imageLiteral(resourceName: "newsletter"),
                               #imageLiteral(resourceName: "notification"),
@@ -26,37 +35,28 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate,UITableViewD
                           "Notifications",
                           "Activation code",
     ]
-    var example = [
-        "afahgfsi af aih fhi ashk fsbaih ihfb abosi bfohb abhsf abkf blab jlf beo b",
-        "a\nb",
-        "a\nb\nc",
-        "a\nb\nc\nd",
-        "a\nb\nc\nd\ne",
-        "a\nb\nc\nd\ne\nf",
-        "a\nb\nc\nd\ne\nf\ng",
-        "a\nb\nc\nd\ne\nf\ng\nh",
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchWeather()
         tableView.delegate = self
         tableView.dataSource = self
         self.navigationController?.interactivePopGestureRecognizer?.delegate = self
         customNavigationBar.delegate = self
-        fetchWeather()
     }
+    
 }
 
 //MARK: Extension for table view
 extension ViewController{
      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return example.count
+        return flag ? 1 : forecastArray.count
     }
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = Bundle.main.loadNibNamed("HeaderView", owner: self, options: nil)?.first as! FirstViewCell
-        if(flag){
+        if flag{
                 headerView.headerImage.image = images[section]
-                    headerView.headerTitle.text = sectionsTitles[section]
+                headerView.headerTitle.text = sectionsTitles[section]
         }
         else {
                 headerView.headerImage.image = images[3]
@@ -66,10 +66,23 @@ extension ViewController{
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! FirstViewCell
-        cell.titleCell.text = "Title"
-        cell.textCell.text = example[indexPath.row]
         cell.textCell.numberOfLines = 0
-        cell.dateCell.text = "01/01/2000"
+        if !flag {
+            cell.titleCell.text = forecastArray[indexPath.row].location
+            cell.dateCell.text = forecastArray[indexPath.row].date
+            let txt:String
+                =   "Max temperature is " + forecastArray[indexPath.row].maxTemp.toString() + "\n" +
+                    "Average temperature is " + forecastArray[indexPath.row].avgTemp.toString() + "\n" +
+                    "Min temperature is " + forecastArray[indexPath.row].minTemp.toString()
+            cell.textCell.text = txt
+        }
+        else{
+            cell.titleCell.text = copyForecast[indexPath.section].location
+            cell.dateCell.text = copyForecast[indexPath.section].date
+            let txt:String
+                =   "Max temperature is " + copyForecast[indexPath.section].maxTemp.toString() + "\n" + "Average temperature is " + copyForecast[indexPath.section].avgTemp.toString() + "\n" + "Min temperature is " + copyForecast[indexPath.section].minTemp.toString()
+            cell.textCell.text = txt
+        }
         return cell
     }
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -84,11 +97,11 @@ extension ViewController{
 extension ViewController{
     func listTap() {
         flag = false
-        tableView.reloadData()
+        reload()
     }
     func checkListTap() {
         flag = true
-        tableView.reloadData()
+        reload()
     }
     
 }
@@ -102,7 +115,7 @@ extension ViewController{
             "q":"Izhevsk",
             "days":3
         ]
-        let request = AF.request("https://api.weatherapi.com/v1/forecast.json",parameters: params, headers: headers).responseJSON{ response in
+        let request = AF.request("https://api.weatherapi.com/v1/forecast.json",parameters: params, headers: headers).responseJSON{ [self] response in
             
             switch response.result{
             case .success(let value):
@@ -111,27 +124,44 @@ extension ViewController{
                 let dates = json["forecast"]["forecastday"].arrayValue.map{$0["date"].stringValue}
                     
                 let maxTemps = json["forecast"]["forecastday"].arrayValue.map{$0["day"]["maxtemp_c"].doubleValue}
-                
                 let minTemps = json["forecast"]["forecastday"].arrayValue.map{$0["day"]["mintemp_c"].doubleValue}
                 let avgTemps = json["forecast"]["forecastday"].arrayValue.map{$0["day"]["avgtemp_c"].doubleValue}
-               
+                let city = json["location"]["name"].stringValue
+                
                 for i in 0..<dates.count{
-                    let tmp:Weather = Weather(date: dates[i], maxTemp: maxTemps[i], minTemp: minTemps[i], avgTemp: avgTemps[i])
+                    let tmp:Weather = Weather(location: city, date: dates[i], maxTemp: maxTemps[i], minTemp: minTemps[i], avgTemp: avgTemps[i])
                     forecastArray.append(tmp)
                     print(forecastArray[i])
+                    
                 }
             case .failure(let error):
                 print(error)
             }
+            DispatchQueue.main.async{
+                copyForecast = forecastArray
+                copyForecast.sort(by: {$0.maxTemp > $1.maxTemp})
+                reload()
+            }
         }
+    }
+    func reload(){
+        self.tableView.reloadData()
+    }
+    
+}
+extension Double {
+    func toString() -> String {
+        return String(format: "%.1f",self)
     }
 }
 struct Weather {
+    var location:String = ""
     var date:String = ""
     var maxTemp:Double = 0.0
     var minTemp:Double = 0.0
     var avgTemp:Double = 0.0
-    init(date: String, maxTemp: Double, minTemp: Double, avgTemp: Double){
+    init(location: String, date: String, maxTemp: Double, minTemp: Double, avgTemp: Double){
+        self.location = location
         self.date = date
         self.maxTemp = maxTemp
         self.minTemp = minTemp
